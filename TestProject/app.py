81fd53d1bffe855e00f7f878254593030b42fa57
Sweta Sharma <69667080+swesh-lab@users.noreply.github.com>
2021-05-22 05:10:01 +0530

    Session included

--- TestProject/app.py
+++ TestProject/app.py
@@ -6,12 +6,27 @@ from flask import Flask, render_template, url_for, request, redirect
 
 
 app = Flask(__name__)
+app.secret_key = "myally"
 
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form["nm"]
+        date_month = request.form["dm"]
+        session["username"] = username
+        return render_template("index.html", result=username)
+    else:
+        return render_template("login.html")
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", result="")
+    if "username" in session:
+        username = session["username"]
+        return render_template("index.html", result=username)
+    else:
+        return redirect(url_for("login"))
 
 
 @app.route('/get')
